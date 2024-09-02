5979d47bffcf3058eed3e52c9f4bffc4577658e7
Dan Davison <dandavison7@gmail.com>
2024-09-02 08:44:58 -0400

    Default to persistent db with conventional location

--- src/tringa/cli.py
+++ src/tringa/cli.py
@@ -5,6 +5,7 @@ from pathlib import Path
 from typing import Annotated, Optional
 
 import typer
+from xdg_base_dirs import xdg_data_home
 
 import tringa.repl
 from tringa import gh, queries
@@ -28,9 +29,13 @@ def global_options(
     db_path: Optional[Path] = None,
     db_type: DBType = DBType.DUCKDB,
 ):
-    """
-    Common options for all commands
-    """
+    if db_path is None:
+        dir = Path(xdg_data_home()) / "tringa"
+        dir.mkdir(parents=True, exist_ok=True)
+        db_path = dir / f"tringa.{db_type.value}"
+    elif db_path == ":memory:":
+        db_path = None
+
     global _global_options
     _global_options = GlobalOptions(db_config=DBConfig(db_type=db_type, path=db_path))
 

a20a1b58d7b7a586448aa27d70dbcffffffb9b07
Dan Davison <dandavison7@gmail.com>
2024-09-22 17:33:25 -0400

    Revert changes to flaky definition

--- src/tringa/annotations/flaky.py
+++ src/tringa/annotations/flaky.py
@@ -2,10 +2,9 @@
 Our definition of a flaky test will probably become more sophisticated over
 time. Our current working definition is:
 
-> A test is flaky if it has failed at least once and it is present on more than
-one branch.
+> A test is flaky if it has failed on more than one branch.
 
-TODO: This definition will give false positives if, for example, the test was
+This definition will give false positives if, for example, the test was
 introduced in feature branch b, and then branch b' was created as a fork of b,
 and it failed in both.
 """
@@ -21,15 +20,13 @@ def annotate(from_db: DB, to_db: DB):
     test_results = Query[TestResult, EmptyParams]("select * from test;").fetchall(
         from_db, {}
     )
-    seen_branches, flaky = defaultdict(set), set()
+    fail_branches, flaky = defaultdict(set), set()
     for tr in test_results:
-        if tr.skipped:
-            continue
         key = (tr.repo, tr.classname, tr.name)
-        seen_branches[key].add(tr.branch)
-        if not tr.passed and len(seen_branches[key]) > 1:
-            flaky.add(key)
-
+        if not tr.passed and not tr.skipped:
+            fail_branches[key].add(tr.branch)
+            if len(fail_branches[key]) > 1:
+                flaky.add(key)
     if any(flaky):
         to_db.connection.executemany(
             "UPDATE test SET flaky = true WHERE repo = ? AND classname = ? AND name = ?",

c174a13b228c0ece779c8f0c7ec3df103756cb3b
Dan Davison <dandavison7@gmail.com>
2024-09-22 08:04:12 -0400

    Evolve flaky definition

--- src/tringa/annotations/flaky.py
+++ src/tringa/annotations/flaky.py
@@ -2,13 +2,12 @@
 Our definition of a flaky test will probably become more sophisticated over
 time. Our current working definition is:
 
-> A test is flaky if it has failed on more than one branch.
+> A test is flaky if it has failed at least once and it is present on more than
+one branch.
 
-This definition will give false positives if, for example, the test was
+TODO: This definition will give false positives if, for example, the test was
 introduced in feature branch b, and then branch b' was created as a fork of b,
 and it failed in both.
-
-TODO: we would like the definition to capture "has failed on main".
 """
 
 from collections import defaultdict
@@ -23,13 +22,14 @@ _query = Query[EmptyParams, tuple[str, str, bool, bool, str]](
 
 def annotate(from_db: DB, to_db: DB):
     tests = _query(from_db, {})
-    fail_branches, flaky = defaultdict(set), set()
+    seen_branches, flaky = defaultdict(set), set()
     for classname, name, passed, skipped, branch in tests:
+        if skipped:
+            continue
         key = (classname, name)
-        if not passed and not skipped:
-            fail_branches[key].add(branch)
-            if len(fail_branches[key]) > 1:
-                flaky.add(key)
+        seen_branches[key].add(branch)
+        if not passed and len(seen_branches[key]) > 1:
+            flaky.add(key)
 
     if any(flaky):
         to_db.connection.executemany(

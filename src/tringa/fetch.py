17143f940efb8903d193241cb465a286cc6fd206
Dan Davison <dandavison7@gmail.com>
2024-09-24 07:54:07 -0400

    [DNW] Support consuming all pages from GitHub API
    tringa(main) uv run tringa repo show --repo temporalio/sdk-python
    Listing remote artifacts matching: ['*junit*', '*xunit*', '*xml*'] for repos: ['temporalio/sdk-python']
    gh api /repos/temporalio/sdk-python/actions/artifacts
    Downloading 30 artifacts junit-xml--11003152835--1--3.12--macos-intel, junit-xml--11003152835--1--3.12--windows-latest, junit-xml--11003152835--1--3.12--ubuntu-latest ...
    ^C
    Aborted.
    ^CException ignored in: <module 'threading' from '/Users/dan/.local/share/uv/python/cpython-3.12.5-macos-aarch64-none/lib/python3.12/threading.py'>
    Traceback (most recent call last):
      File "/Users/dan/.local/share/uv/python/cpython-3.12.5-macos-aarch64-none/lib/python3.12/threading.py", line 1624, in _shutdown
        lock.acquire()
    KeyboardInterrupt:

--- src/tringa/fetch.py
+++ src/tringa/fetch.py
@@ -76,7 +76,9 @@ async def _list_remote_artifacts_for_repo(repo: str) -> list[Artifact]:
             "branch": artifact["workflow_run"]["head_branch"],
             "commit": artifact["workflow_run"]["head_sha"],
         }
-        for artifact in (await gh.api(f"/repos/{repo}/actions/artifacts"))["artifacts"]
+        for artifact in (await gh.api(f"/repos/{repo}/actions/artifacts"))[0][
+            "artifacts"
+        ]
     ]
 
 

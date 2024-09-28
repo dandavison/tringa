2341259c7e15e1c06cd75cb588e5ea3187e5a6a2
Dan Davison <dandavison7@gmail.com>
2024-09-28 06:47:21 -0400

    Evolve Run model

--- src/tringa/cli/run/show.py
+++ src/tringa/cli/run/show.py
@@ -35,7 +35,7 @@ class Report(reports.Report):
             yield ("PR", pr)
             yield (
                 "Last run",
-                f"[link={self.run.url}]{humanize.naturaltime(self.run.time)}[/link]",
+                f"[link={self.run.url}]{humanize.naturaltime(self.run.started_at)}[/link]",
             )
             yield (
                 "Failed tests",

89f94f39e57ca22ed4be81296fdaa90f6d09d017
Dan Davison <dandavison7@gmail.com>
2024-10-08 07:02:11 -0400

    Bug fix: add uniqueness constraints

--- src/tringa/db.py
+++ src/tringa/db.py
@@ -36,7 +36,18 @@ CREATE TABLE test (
     skipped BOOLEAN,
     flaky BOOLEAN,
     message VARCHAR,
-    text VARCHAR
+    text VARCHAR,
+
+    -- A run may have multiple run attempts. The artifact name typically includes
+    -- the run attempt number, in order to avoid artifact name conflicts. However,
+    -- GitHub does not expose the run attempt number in the metadata associated with
+    -- an artifact, and we follow suite: i.e. we demand uniqueness on the following
+    -- tuple, which means that multiple artifacts for the same run may not coexist
+    -- in the table.
+    PRIMARY KEY (repo, run_id, file, suite, classname, name),
+
+    -- The following should be true also.
+    -- UNIQUE (repo, artifact, file, suite, classname, name)
 );
 """
 
@@ -58,11 +69,21 @@ class DB:
         # Inserting columns from a dataframe is more efficient than inserting
         # rows from a SQL INSERT statement.
         n_rows = str(len(rows)) if isinstance(rows, Sequence) else "<iterator>"
+
         df = pd.DataFrame(rows)
         if df.empty:
             return
         debug(f"Inserting {n_rows} rows into {self}")
-        self.connection.execute("insert into test select * from df")
+        # Sort by time so that rows from later run attempts (that match on the
+        # uniqueness constraints) replace those from earlier run attempts.
+        self.connection.execute(
+            """
+            INSERT OR REPLACE INTO test
+            SELECT DISTINCT ON (repo, run_id, file, suite, classname, name) *
+            FROM df
+            ORDER BY repo, run_id, file, suite, classname, name, suite_time DESC
+            """
+        )
 
     def fetchone(self, sql: str) -> Any:
         rows = self.connection.execute(sql).fetchall()
